44b2c0d41afca822aa74be43d97b7dad7cc54b9d
lucas schmitz <lucas.schmitz@institutoivoti.com.br>
2017-10-10 19:23:09 -0300

    Adicionado teste nome do funcionario

--- src/br/com/herança/Teste.java
+++ src/br/com/herança/Teste.java
@@ -7,23 +7,32 @@ import org.junit.Test;
 public class Teste {
 
 	@Test
-	public void test() {
+	public void testSenhaTrue() {
 		Gerente gerente = new Gerente();
 		gerente.Senha(5555);
 		assertEquals(gerente.autentica(5555), true);
 	}
 
 	@Test
-	public void test1() {
+	public void testSenhaFalse() {
 		Gerente gerente = new Gerente();
 		gerente.Senha(5555);
 		assertEquals(gerente.autentica(4545), false);
 	}
 
 	@Test
-	public void test2() {
+	public void testNome() {
 		Gerente gerente = new Gerente();
 		gerente.Nome("grovy");
 		assertEquals(gerente.GetNome(), "grovy");
 	}
+
+	@Test
+	public void testNomeFuncionario() {
+		Funcionario funcionario = new Funcionario();
+		funcionario.Nome("Irma");
+		assertEquals(funcionario.GetNome(), "Irma");
+
+	}
+
 }
